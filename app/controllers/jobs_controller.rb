08b02bc6a71d9d2003773d30aa4e254428eb1fe2
freiwillwn <fr.willen@gmail.com>
2012-03-21 09:23:07 +0300

    Now can log collaborators jobs from project page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,14 @@ class JobsController < SecureController
   #belongs_to :project
   
   def create
+    @project = if request.referer.include? user_other_projects_path(current_user)
+      params[:job][:person_id] = current_user.id
+      current_user.other_projects
+     elsif request.referer.include? user_projects_path(current_user)
+      current_user.projects  
+    end.find params[:job][:project_id]
+    params[:job].delete(:project_id)
+    @job = @project.jobs.create params[:job]
     create!  do |format|
       format.js {}
     end

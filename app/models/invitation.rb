b2bb7edfe569f46f28d897d3d9555533974d980d
Kostya <kmalyn@softserveinc.com>
2012-04-03 20:54:42 +0300

    Some adjustments to create and confirm invitations functionality

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,12 +1,12 @@
 class Invitation < ActiveRecord::Base
-  
+
   belongs_to :project
   before_save :generate_key, :set_expires_at, :set_initial_password
   after_save :send_email
   validates :email, :presence => true
   validates :rate, :numericality => true
 
-  def generate_key    
+  def generate_key
     k = []
     15.times do
       k << rand(9)
@@ -27,10 +27,15 @@ class Invitation < ActiveRecord::Base
   end
 
   def confirm
-    @project_owner = User.find project.user_id
-    @user = User.create(:email => self.email, :password => self.password, :password_confirmation => self.password, :first_name => self.first_name, :last_name => self.last_name, :address => "Your City", :description => '', :hourly_rate => self.rate)
-    @project_owner.people << @user.as_person
-    ProjectStaff.create(:person_id => @user.id, :project_id => self.project_id, :description => '')
+    project_owner = User.find project.user_id
+
+    user = User.find_by_email(self.email) || User.create(:email => self.email, :password => self.password, :password_confirmation => self.password, :first_name => self.first_name, :last_name => self.last_name, :address => "Your City", :description => '', :hourly_rate => self.rate)
+
+
+
+    person = user.as_person
+    project_owner.people << person #unless project_owner.people.include? person
+    ProjectStaff.create(:person_id => user.id, :project_id => self.project_id, :description => '') unless project.people.include? person
     destroy
   end
 

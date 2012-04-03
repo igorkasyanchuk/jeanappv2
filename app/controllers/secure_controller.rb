1ca18a16c4673d78cb3ded65fbb0b7e048367b9c
Kostya <kmalyn@softserveinc.com>
2012-04-03 19:37:51 +0300

    Updated sorting for project#jobs. Restored 1.9.3 fix for SecureController#update_project_details

--- app/controllers/secure_controller.rb
+++ app/controllers/secure_controller.rb
@@ -1,20 +1,20 @@
 class SecureController < InheritedResources::Base
   include ActionView::Helpers::JavaScriptHelper
   include ActionView::Helpers::NumberHelper
-  
+
   respond_to :html, :xml, :json
-  
+
   before_filter :require_user
   before_filter :check_user_permissions
-  
+
   layout 'admin'
 
   helper_method :refresh_diagram
   helper_method :clear_fields
   helper_method :update_project_details
-  
+
   private
-    
+
     def refresh_diagram(project)
       script =<<EOF
 $.colorbox.close();
@@ -22,24 +22,24 @@ setTimeout("refreshChart([#{project.profit},#{project.amount_total},#{project.pr
 EOF
       script.html_safe
     end
-    
+
     def clear_fields
       script =<<EOF
         $('form.formtastic input[type=text], form.formtastic textarea').val('');
 EOF
       script.html_safe
     end
-    
+
     def update_project_details(project)
       script =<<EOF
-        $ee = "#{escape_javascript((render '/projects/_info', :locals => {:project => project}))}";
+        $ee = "#{escape_javascript((render '/projects/_info', :locals => {:project => project}).join)}";
         $('#info').html($ee);
         show_money_chart(#{@project.money_chart.to_json});
         show_time_chart(#{@project.time_chart.to_json});
 EOF
       script.html_safe
     end
-    
+
     def check_user_permissions
       if params[:user_id]
         if current_user != User.find_by_id(params[:user_id])
@@ -48,5 +48,5 @@ EOF
         end
       end
     end
-  
+
 end
